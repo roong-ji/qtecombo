01875c8ff79e27be56d3fbeb24a947c851a6728c
roong-ji <mhienegjyuu@naver.com>
2025-11-18 17:35:11 +0900

    fix: 밸런스 수정

--- Assets/02.Scripts/Manager/GameManager.cs
+++ Assets/02.Scripts/Manager/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -21,8 +22,39 @@ public class GameManager : MonoBehaviour
     private bool _isGameOver = false;
     public bool IsGameOver => _isGameOver;
 
+    [SerializeField] private GameObject _startButton;
+    [SerializeField] private GameObject _restartButton;
+    [SerializeField] private GameObject _line;
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+        _restartButton.SetActive(false);
+        _line.SetActive(false);
+    }
+
     public void GameOver()
     {
         _isGameOver = true;
+        Invoke(nameof(ButtonOn), 3f);
+        _line.SetActive(false);
+    }
+
+    public void ButtonOn()
+    {
+        _restartButton.SetActive(true);
+    }
+
+    public void GameStart()
+    {
+        Time.timeScale = 1f;
+        _startButton.SetActive(false);
+        _line.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
